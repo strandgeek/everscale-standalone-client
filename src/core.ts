9f1a50d865291cf4c4d761e6b5ebd685bfdb2b90
Carlos Strand <carlosstrand@gmail.com>
2023-06-07 23:37:47 -0400

    feat: fix undefined ensureNekotonLoaded for Vite production build

--- src/core.ts
+++ src/core.ts
@@ -1,4 +1,5 @@
 import type * as nt from 'nekoton-wasm';
+import init, * as nekoton from 'nekoton-wasm';
 
 const core = {
   ensureNekotonLoaded: undefined as unknown,
@@ -12,6 +13,25 @@ const core = {
   fetch: typeof fetch,
   fetchAgent: (url: string) => any,
   debugLog: (...data: any[]) => void,
+};
+
+let clientInitializationStarted = false;
+let notifyClientInitialized: { resolve: () => void; reject: () => void };
+const initializationPromise: Promise<void> = new Promise<void>((resolve, reject) => {
+  notifyClientInitialized = { resolve, reject };
+});
+
+core.ensureNekotonLoaded = (initInput?: nt.InitInput | Promise<nt.InitInput>): Promise<void> => {
+  if (!clientInitializationStarted) {
+    clientInitializationStarted = true;
+    init(initInput).then(notifyClientInitialized.resolve).catch(notifyClientInitialized.reject);
+  }
+  return initializationPromise;
+};
+core.nekoton = nekoton;
+core.fetch = fetch;
+core.debugLog = _nothing => {
+  /* do nothing */
 };
 
 export default core;
